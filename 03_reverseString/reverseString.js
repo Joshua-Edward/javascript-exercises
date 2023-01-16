5aa6ca6fe25d18de348a2d4730eef4ed464a45ca
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-01-15 18:07:58 -0800

    Begin exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  let reversedString = '';
+  if (inputString.length === 0) {
+    reversedString = '';
+  } else {
+      for (let i = 1; i < (inputString.length); i++) {
+        reversedString += inputString.charAt(inputString.length - i);
+      }
+  }
+  return(reversedString);
 };
 
 // Do not edit below this line
