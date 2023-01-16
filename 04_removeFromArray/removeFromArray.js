17672991599011d72cb950ca1b4e47ba580c3a8e
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-01-16 13:13:05 -0800

    Complete exercised 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,18 +3,12 @@ const removeFromArray = function (inputArray) {
   let elementLocation = 0;
   for (let i = 1; i < arguments.length; i++) {
     if (changedArray.includes(arguments[i])) {
-      elementLocation = changedArray.findIndex(() => arguments[i]);
-      console.log(`test ${arguments[i]}`);
-      changedArray = changedArray.slice(elementLocation, elementLocation + 1);
-    } else {
-      changedArray = changedArray;
+      elementLocation = changedArray.indexOf(arguments[i]);
+      changedArray.splice(elementLocation, 1);
     } 
   }
-  console.log(changedArray);
   return(changedArray);
 };
-
-removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
