73a0a58c01ce51d5d45b9877823af306b4a584a3
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-01-16 13:56:38 -0800

    Complete exercises 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(var1, var2) {
+  let sum = 0;
+  
+  if (var1 < 0 || var2 < 0) {
+    sum = 'ERROR';
+  } else if (typeof(var1) != "number" || typeof(var2) != "number") {
+    sum = 'ERROR';
+  } else if (var1 <= var2) {
+    for (let i = var1; i <= var2; i++)
+    sum += i;
+  } else if (var1 > var2) {
+    for (let i = var1; i >= var2; i--)
+    sum += i;
+  } 
+  return(sum)
 };
 
 // Do not edit below this line
