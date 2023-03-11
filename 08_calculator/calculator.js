7eab4b61373a7a2276923017f52b24d78d5e96f6
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-03-10 20:52:13 -0800

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let addTotal = a + b;
+  return addTotal;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let subtractTotal = a - b;
+  return subtractTotal;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	sumArrayTotal = 0;
+  for (let i = 0; i < sumArray.length; i++) {
+    sumArrayTotal += sumArray[i];
+  }
+  return sumArrayTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+	multiplyArrayTotal = 1;
+  for (let i = 0; i < multiplyArray.length; i++) {
+    multiplyArrayTotal *= multiplyArray[i];
+  }
+  return multiplyArrayTotal;
 };
 
-const power = function() {
-	
+const power = function(baseNumber, powerNumber) {
+  powerTotal = baseNumber;
+  for (let i = 1; i < powerNumber; i++) {
+    powerTotal *= baseNumber;
+  }
+  return powerTotal;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  factorialTotal = 1;
+  if (a === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= a; i++) {
+      factorialTotal *= i;
+    }
+    return factorialTotal;
+  }
 };
 
 // Do not edit below this line
