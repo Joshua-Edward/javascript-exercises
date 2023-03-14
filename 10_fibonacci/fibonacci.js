fbb84143ba6709e92a2a5cba0ba4f608d85f6611
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-03-13 21:51:16 -0700

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNumber) {
+  parsedFib = parseInt(fibNumber);
+  let fibArray = [1, 1];
+  if ((typeof parsedFib) !== "number") {
+    return "OOOPS";
+  } else if (parsedFib <= 0) {
+    return "OOPS"
+  } else if (parsedFib === 1 || parsedFib === 2) {
+    return 1;
+  } else {
+    for (i = 2; i < parsedFib; i++) {
+      fibArray.push(fibArray[i-1]+fibArray[i-2]);
+    };
+  };
+  return fibArray.pop();
 };
 
 // Do not edit below this line
