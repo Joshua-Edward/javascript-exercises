e2bd748608c68e89feb4901507b965cfa5d5ea93
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-03-13 20:59:28 -0700

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (originalString) {
+  const condensedString = originalString.replace(/[^a-xA-Z]/g, '').toLowerCase();
+  const stringLength = condensedString.length;
+  let testResults;
+  for (i = 0; i < stringLength/2; i++) {
+    if (condensedString[i] === condensedString[stringLength-(i+1)]) {
+    testResults = true;
+    } else {
+      testResults = false;
+    };
+  };
+  return testResults;
 };
+
+
+/* Answer from solution sheet using array methods
+const palindromes = function (string) {
+  const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return processedString.split("").reverse().join("") == processedString;
+};
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
