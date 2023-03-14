b38f55eb442deba8e8f0409affbee979b242249e
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-03-13 22:00:30 -0700

    Complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  booksArray = [];
+  for (i = 0; i < 2; i++) {
+    booksArray.push(books[i].title)
+  };
+  return booksArray;
 };
 
 // Do not edit below this line
