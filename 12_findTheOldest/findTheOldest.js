5cf401fddcc543e1f45225e3c222b93d04b7ee92
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-03-14 20:56:26 -0700

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = (function(peopleArray) {
+  peopleArray.sort((firstPerson, secondPerson) => {
+    const yearNow = new Date().getFullYear();
+    let lastGuy = 0;
+    if ("yearOfDeath" in firstPerson) {
+      lastGuy = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+    } else {
+      lastGuy = yearNow - firstPerson.yearOfBirth;
+    };
+    let nextGuy = 0;
+    if ("yearOfDeath" in secondPerson) {
+      nextGuy = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+    } else {
+      nextGuy = yearNow - secondPerson.yearOfBirth;
+    };
+    return lastGuy > nextGuy ? -1 : 1;
+  });
+  return peopleArray[0];
+});
 
 // Do not edit below this line
 module.exports = findTheOldest;
